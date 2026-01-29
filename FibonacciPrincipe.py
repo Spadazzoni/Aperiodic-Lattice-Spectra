a1298d559a0d2f6e41f6c083396551fafe8ebae1
giulio <giulio.spadaro@studio.unibo.it>
2026-01-29 18:00:37 +0100

    Added the main for the program. Not satisfying.

---
+++ FibonacciPrincipe.py
@@ -0,0 +1,32 @@
+import FibonacciLattice as fl
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    N = 10
+    Seed = "0,0"
+    Size = 70
+    Mass = 1
+    Translations = 50
+    TimeSteps = 100
+
+    FullSpectrum = fl.FullSpectrum(Size, Mass, Seed, N, TimeSteps, Translations)
+    Map = np.outer(range(Translations * TimeSteps), np.ones(Size)/TimeSteps)
+    fig, ax = plt.subplots(figsize=(25, 25))
+
+    ax.scatter(
+        Map,
+        FullSpectrum,
+        marker=".",
+        s=1,
+        linewidths=0.5,
+        color="black",
+        rasterized=True,
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
